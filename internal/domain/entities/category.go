e61b801112b3ec968a62808627b3b6e4b5165c9c
Andrei Santos <andrei.as3@hotmail.com>
2024-10-22 23:19:26 -0300

    refactor: rename fields private

--- internal/domain/entities/category.go
+++ internal/domain/entities/category.go
@@ -1,9 +1,77 @@
 package entities
 
+import err "github.com/andreis3/catalog-write-api/internal/domain/errors"
+
 type Category struct {
-	ID          int64
-	CategoryID  int64
-	Slug        string
-	Description string
-	Parent      string
+	id                int64
+	categoryKey       string
+	description       string
+	parentID          int64
+	parentCategoryKey string
+	err.EntityErrors
+}
+
+func CategoryBuilder() *Category {
+	return &Category{}
+}
+
+func (c *Category) GetID() int64 {
+	return c.id
+}
+
+func (c *Category) GetCategoryKey() string {
+	return c.categoryKey
+}
+
+func (c *Category) GetDescription() string {
+	return c.description
+}
+
+func (c *Category) GetParentID() int64 {
+	return c.parentID
+}
+
+func (c *Category) GetParentCategoryKey() string {
+	return c.parentCategoryKey
+}
+
+func (c *Category) SetID(id int64) *Category {
+	c.id = id
+	return c
+}
+
+func (c *Category) SetCategoryKey(categoryKey string) *Category {
+	c.categoryKey = categoryKey
+	return c
+}
+
+func (c *Category) SetDescription(description string) *Category {
+	c.description = description
+	return c
+}
+
+func (c *Category) SetParentID(parentID int64) *Category {
+	c.parentID = parentID
+	return c
+}
+
+func (c *Category) SetParentCategoryKey(parentCategoryKey string) *Category {
+	c.parentCategoryKey = parentCategoryKey
+	return c
+}
+
+func (c *Category) Build() *Category {
+	return c
+}
+
+func (c *Category) Validate() *err.EntityErrors {
+	if c.categoryKey == "" {
+		c.Add("category_key: is required")
+	}
+
+	if c.description == "" {
+		c.Add("description: is required")
+	}
+
+	return &c.EntityErrors
 }
